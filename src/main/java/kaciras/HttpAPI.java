1c34846fd68defa1dbdc2f63f959507858ad51bd
Kaciras <Kaciras@protonmail.com>
2021-05-04 19:22:31 +0800

    (WIP) serve static files

--- src/main/java/kaciras/HttpAPI.java
+++ src/main/java/kaciras/HttpAPI.java
@@ -4,19 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 import org.apache.ibatis.reflection.ParamNameResolver;
 import org.apache.ibatis.session.Configuration;
 
 import java.io.IOException;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -30,19 +25,11 @@ public final class HttpAPI implements HttpHandler {
 		public final Object data;
 	}
 
-	private final Map<Class<? extends Annotation>, Function<Annotation, String[]>> sqlProviders = Map.of(
-			Insert.class, a -> ((Insert) a).value(),
-			Delete.class, a -> ((Delete) a).value(),
-			Update.class, a -> ((Update) a).value(),
-			Select.class, a -> ((Select) a).value()
-	);
-
 	private final ObjectMapper objectMapper = new ObjectMapper();
 
 	private final Configuration config;
 	private final CategoryMapper sqlMapper;
 
-	private final Map<Method, String[]> sqlTable = new IdentityHashMap<>();
 	private final Map<String, Method> methodTable;
 
 	public HttpAPI(Configuration config, CategoryMapper sqlMapper) {
@@ -50,22 +37,40 @@ public final class HttpAPI implements HttpHandler {
 		this.sqlMapper = sqlMapper;
 
 		methodTable = Arrays.stream(CategoryMapper.class.getMethods())
-				.peek(this::collectSql)
 				.collect(Collectors.toMap(Method::getName, Function.identity()));
-	}
-
-	private void collectSql(Method method) {
-		for (var e : sqlProviders.entrySet()) {
-			var annotation = method.getDeclaredAnnotation(e.getKey());
-			if (annotation != null) {
-				sqlTable.put(method, e.getValue().apply(annotation));
-				return;
-			}
-		}
 	}
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
+		var endpoint = exchange.getRequestURI().getPath();
+
+		if (endpoint.startsWith("/api")) {
+			invokeSQL(exchange);
+		} else {
+			sendFile(exchange, endpoint);
+		}
+
+		exchange.close();
+	}
+
+	private void sendFile(HttpExchange exchange, String name) throws IOException {
+		if ("/".equals(name)) {
+			name = "index.html";
+		}
+		var path = Path.of("web", name);
+
+		// probeContentType 对 .js 返回 text/plain？
+		var mime = Files.probeContentType(path);
+		if (name.endsWith(".js")) {
+			mime = "application/javascript";
+		}
+		exchange.getResponseHeaders().add("Content-Type", mime);
+
+		exchange.sendResponseHeaders(200, Files.size(path));
+		Files.copy(path, exchange.getResponseBody());
+	}
+
+	private void invokeSQL(HttpExchange exchange) throws IOException {
 		var endpoint = exchange.getRequestURI().getPath().substring(1);
 		var method = methodTable.get(endpoint);
 
@@ -86,7 +91,6 @@ public final class HttpAPI implements HttpHandler {
 		var pns = new ParamNameResolver(config, method);
 		var boundSql = config.getMappedStatement(endpoint).getBoundSql(pns.getNamedParams(args));
 
-
 		try {
 			var start = System.currentTimeMillis();
 			var data = method.invoke(sqlMapper, args);
@@ -100,16 +104,5 @@ public final class HttpAPI implements HttpHandler {
 			e.printStackTrace();
 			exchange.sendResponseHeaders(500, 0);
 		}
-
-		exchange.close();
-	}
-
-	@SuppressWarnings("unchecked")
-	private static Map<String, String> getParams(URI uri) {
-		var segments = uri.getQuery().split("&");
-		var entries = Arrays.stream(segments)
-				.map(s -> s.split("=", 2))
-				.map(s -> Map.entry(s[0], s[1]));
-		return Map.ofEntries(entries.toArray(Map.Entry[]::new));
 	}
 }
--- src/main/java/kaciras/Main.java
+++ src/main/java/kaciras/Main.java
@@ -3,10 +3,12 @@ package kaciras;
 import com.sun.net.httpserver.HttpServer;
 import org.apache.ibatis.session.SqlSession;
 
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 public final class Main {
+
+	private static final String HOST_NAME = "localhost";
+	private static final int PORT = 6666;
 
 	private static SqlSession session;
 
@@ -19,14 +21,14 @@ public final class Main {
 
 		// 如果使用Spring，可以用@Configurable来注入此依赖。
 		Category.categoryMapper = mapper;
-
-		var server = HttpServer.create();
+		var server = HttpServer.create(new InetSocketAddress(HOST_NAME, PORT), 0);
 		server.createContext("/", new HttpAPI(session.getConfiguration(), mapper));
-		server.bind(new InetSocketAddress(InetAddress.getLocalHost(), 6666), 0);
 		server.start();
 
 		// 玩完记得把表删了
 		Runnable cleanup = () -> Utils.dropTables(session.getConnection());
 		Runtime.getRuntime().addShutdownHook(new Thread(cleanup));
+
+		System.out.println("Demo hosted on http://" + HOST_NAME + ":" + PORT);
 	}
 }
