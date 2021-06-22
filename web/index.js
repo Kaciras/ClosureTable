e9cc124190c09e1dbece4f02f38b560fee38c8af
Kaciras <Kaciras@protonmail.com>
2021-06-22 20:33:47 +0800

    (WIP) refactor web page

--- web/index.js
+++ web/index.js
@@ -1,36 +1,127 @@
-async function invokeSQL(name, ...args) {
-	const response = await fetch(`http://localhost:6666/${name}`, {
+import { addButton, updateList, updateMetadata } from "./workspace.js";
+import { update } from "./tree.js";
+
+const API = "http://localhost:6666/api/";
+
+function invokeSQL(name, args = {}) {
+	const request = new Request(API + name, {
 		method: "POST",
 		body: JSON.stringify(args),
 	});
-	return await response.json();
+	return fetch(request).then(r => r.json());
 }
 
-function showNodes(list) {
-	const [tBody] = document.getElementById("table").tBodies;
+const tabTemplate = document.getElementById("tab-template");
 
-	for (const item of list) {
-		const tRow = document.createElement("tr");
-
-		const id = document.createElement("td");
-		tRow.append(id);
-		id.textContent = item.id;
-
-		const name = document.createElement("td");
-		tRow.append(name);
-		name.textContent = item.name;
-
-		tBody.append(tRow);
-	}
+const Type = {
+	Modify: 0,
+	QueryValue: 1,
+	QueryList: 2,
 }
+
+const buttons = [
+	{
+		content: "查询全部分类",
+
+	},
+	{
+		content: "查询全部分类",
+		type: Type.QueryList,
+		async onClick() {
+			const { sql, time, data } = await invokeSQL("getPath", { id: 10, ancestor: 1 });
+			updateList(data);
+			updateMetadata({ sql, time, data });
+		}
+	},
+	{
+		content: "分类间的路径",
+		async onClick() {
+			addButton();
+		}
+	},
+	{
+		content: "查询分类的级别"
+
+	},
+];
 
 const container = document.getElementById("console");
 const button = document.createElement("button");
 button.textContent = "查询全部分类";
 button.onclick = async () => {
-	const { sql, time, data } = invokeSQL("selectDescendant", 0);
-	showNodes(data);
-	document.getElementById("time").textContent = time;
-	document.getElementById("sql").textContent = sql;
+	addButton();
+	// const { sql, time, data } = await invokeSQL("getPath", 10, 1);
+	//
+	// refreshTable(data);
+	// refreshTreeView(data);
+	//
+	// document.getElementById("time").textContent = time;
+	// document.getElementById("sql").textContent = sql;
 };
 container.append(button);
+
+invokeSQL("getAll").then(r => update(r.data));
+
+// 虽然纯 CSS 也能做 TabPanel，但比 JS 麻烦所以不用
+class ConsoleTabPanels {
+
+	constructor() {
+		this.buttons = [];
+	}
+
+	addTab(name, panel) {
+		const index = this.buttons.length;
+
+
+		this.buttons.push(button);
+	}
+
+	mount() {
+		this.buttons[0].firstElementChild.checked = true;
+		this.buttons[0].classList.add("active");
+
+		const div = document.getElementById("console-head");
+		this.buttons.forEach(b => div.append(b));
+	}
+}
+
+const tabMap = {};
+let currentTab;
+
+function addTab(name, api) {
+	const radio = document.createElement("input");
+	radio.type = "radio";
+	radio.name = "tab";
+	radio.className = "hide";
+
+	const button = document.createElement("label");
+	button.className = "tab-button";
+	button.textContent = name;
+	button.append(radio);
+
+	const panel = document.getElementById(api);
+
+	radio.addEventListener("change", () => {
+		panel.classList.add("active");
+		button.classList.add("active");
+		currentTab.button.classList.remove("active");
+		currentTab.panel.classList.remove("active");
+		currentTab = tabMap[name];
+	});
+
+	tabMap[name] = { button, panel };
+	document.getElementById("console-head").append(button);
+}
+
+addTab("插入", "create");
+addTab("更新", "update");
+addTab("删除", "delete");
+addTab("移动", "move");
+addTab("查询级别", "getLevel");
+addTab("查询路径", "getPath");
+addTab("查询子层", "getSubLayer");
+addTab("查询子树", "getTree");
+
+currentTab = tabMap["查询路径"];
+currentTab.button.classList.add("active");
+currentTab.panel.classList.add("active");
--- web/tree.js
+++ web/tree.js
@@ -1,46 +1,34 @@
-const treeData = {
-	"name": "Top Level",
-	"children": [
-		{
-			"name": "Level 2: A",
-			"children": [
-				{ "name": "Son of A" },
-				{ "name": "Daughter of A" },
-			]
-		},
-		{ "name": "Level 2: B" },
-	]
-};
-
-const treeData2 = {
-	"name": "Top Level",
-	"children": [
-		{
-			"name": "Level 2: A",
-		},
-		{ "name": "Son of A" },
-		{ "name": "Daughter of A" },
-		{ "name": "Level 2: B" },
-	]
-};
-
 let svg;
 
-update(treeData)
+const container = document.getElementById("tree-view");
 
-export function update(list = treeData2) {
+/**
+ *
+ * @param list
+ * @see https://github.com/xswei/d3-hierarchy/blob/master/README.md
+ */
+export function update(list ) {
 	if (svg) {
 		svg.remove();
 	}
 	svg = d3.select("#tree-view")
 		.append("svg")
-		.attr("width", 700)
-		.attr("height", 600);
+		.attr("width", container.clientWidth)
+		.attr("height", container.clientHeight);
+
+	const zoom = d3.zoom()
+		.scaleExtent([1, 8])
+		.on('zoom', event => g.attr('transform', event.transform));
+
+	svg.call(zoom);
 
 	const g = svg.append("g");
 
+	/*
+	 * stratify() 能将列表转为层次结构数据。
+	 */
 	const treemap = d3.tree().size([700, 600]);
-	const nodes = treemap(d3.hierarchy(list));
+	const nodes = treemap(d3.stratify()(list));
 
 	const link = g.selectAll(".link")
 		.data(nodes.descendants().slice(1))
@@ -66,10 +54,4 @@ export function update(list = treeData2) {
 		.attr("y", d => d.children ? -20 : 20)
 		.style("text-anchor", "middle")
 		.text(d => d.data.name);
-
-	const zoom = d3.zoom()
-		.scaleExtent([1, 8])
-		.on('zoom', event => g.attr('transform', event.transform));
-
-	svg.call(zoom);
 }
