f22ba18bf0cd4507b255f8efdf5712f8b929be27
Kaciras <Kaciras@protonmail.com>
2022-04-19 14:35:47 +0800

    Using `SimpleFileServer` introduced in JAVA 18.

--- src/main/java/kaciras/Main.java
+++ src/main/java/kaciras/Main.java
@@ -3,42 +3,15 @@ package kaciras;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.SimpleFileServer;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 public final class Main {
 
 	private static final String HOST_NAME = "localhost";
 	private static final int PORT = 6666;
-
-	public static void main(String[] args) throws Exception {
-		System.setProperty("file.encoding", "UTF-8");
-
-		var manager = DBManager.open();
-		manager.importData();
-
-		var tracked = new TrackingDataSource(manager.getDataSource());
-		var session = Utils.createSqlSession(tracked);
-
-		var mapper = session.getMapper(CategoryMapper.class);
-		Category.mapper = mapper;
-		var controller = new Controller(new Repository(mapper));
-		var api = new HttpAdapter(tracked, session, controller);
-
-		var server = HttpServer.create(new InetSocketAddress(HOST_NAME, PORT), 0);
-		server.createContext("/api/", wrapHandler(api));
-		server.createContext("/", wrapHandler(Main::serveFiles));
-		server.start();
-
-		// 玩完记得把表删了
-		Runtime.getRuntime().addShutdownHook(new Thread(manager::dropTables));
-
-		System.out.println("Demo hosted on http://" + HOST_NAME + ":" + PORT);
-	}
 
 	/**
 	 * 包装一个 HttpHandler，增加异常处理和自动关闭 HttpExchange 功能，
@@ -58,30 +31,30 @@ public final class Main {
 		};
 	}
 
-	private static void serveFiles(HttpExchange exchange) throws IOException {
-		var name = exchange.getRequestURI().getPath();
-		if ("/".equals(name)) {
-			name = "index.html";
-		}
-		var path = Path.of("web", name);
+	public static void main(String[] args) throws Exception {
+		System.setProperty("file.encoding", "UTF-8");
 
-		try {
-			// 先检查下文件是否存在，JS 的扩展名容易漏。
-			var size = Files.size(path);
+		var manager = DBManager.open();
+		manager.importData();
 
-			// probeContentType 对 .js 返回错误的 text/plain
-			var mime = Files.probeContentType(path);
-			if (name.endsWith(".js")) {
-				mime = "application/javascript";
-			}
-			exchange.getResponseHeaders().add("Content-Type", mime);
+		// 玩完记得把表删了
+		Runtime.getRuntime().addShutdownHook(new Thread(manager::dropTables));
 
-			exchange.sendResponseHeaders(200, size);
-			Files.copy(path, exchange.getResponseBody());
-		} catch (NoSuchFileException ex) {
-			exchange.sendResponseHeaders(404, 0);
-		} catch (IOException ignore) {
-			// 客户端断开会抛“你的主机中的软件中止了一个已建立的连接”
-		}
+		var tracked = new TrackingDataSource(manager.getDataSource());
+		var session = Utils.createSqlSession(tracked);
+
+		var mapper = session.getMapper(CategoryMapper.class);
+		Category.mapper = mapper;
+		var controller = new Controller(new Repository(mapper));
+		var api = new HttpAdapter(tracked, session, controller);
+
+		var server = HttpServer.create(new InetSocketAddress(HOST_NAME, PORT), 0);
+		server.createContext("/api/", wrapHandler(api));
+
+		var wwwRoot = Path.of("web").toAbsolutePath();
+		server.createContext("/", SimpleFileServer.createFileHandler(wwwRoot));
+
+		server.start();
+		System.out.println("Demo hosted on http://" + HOST_NAME + ":" + PORT);
 	}
 }
